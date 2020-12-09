6b23ff0001fce63b18ff3e8afbb30df81365172f
Ronison-GIC <ronison.silva@gicbrasil.com.br>
2020-12-09 08:29:35 -0400

    Implementações adicionais

--- mudi/src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
+++ mudi/src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
@@ -81,5 +81,12 @@ public class Pedido {
 	public void setStatus(StatusPedido status) {
 		this.status = status;
 	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
 	
 }
